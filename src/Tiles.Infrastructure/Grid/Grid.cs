b09f4a1d5fa6106222b80f5d666ddc03852fce8e
Joey  Brown <jbrown@gmcps.com>
2019-06-20 00:01:36 -0500

    working on cut and lay service

--- src/Tiles.Infrastructure/Grid/Grid.cs
+++ src/Tiles.Infrastructure/Grid/Grid.cs
@@ -5,11 +5,13 @@ namespace Tiles.Infrastructure.Grid
 {
   public class Grid
   {
+    private int TileWidth = 0;
     private GridElement[] Elements { get; } = new GridElement[0];
 
-    public Grid(IEnumerable<GridElement> elements)
+    public Grid(IEnumerable<GridElement> elements, int tileWidth)
     {
       Elements = elements.ToArray();
+      TileWidth = tileWidth;
     }
 
     public Grid()
---
+++ src/Tiles.Infrastructure/Lay/ITileLayService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiles.Infrastructure.Grid;
+
+namespace Tiles.Infrastructure.Lay
+{
+  public interface ITileLayService
+  {
+    Task<Image> LayTile(Grid.Grid grid);
+  }
+
+  public class TileLayService : ITileLayService
+  {
+    public static void CopyRegionIntoImage(Bitmap srcBitmap, Rectangle srcRegion, ref Bitmap destBitmap, Rectangle destRegion)
+    {
+      using (var grD = Graphics.FromImage(destBitmap))
+      {
+        grD.DrawImage(srcBitmap, destRegion, srcRegion, GraphicsUnit.Pixel);
+      }
+    }
+
+    /// <inheritdoc />
+    public async Task<Image> LayTile(Grid.Grid grid)
+    {
+      var elements = grid.GetElements().ToArray();
+      if (!elements.Any())
+        throw new Exception("No tiles to lay. Cannot return empty image");
+      var tileWidth = new Bitmap(elements.First().Value).Width;
+      var width = elements.Select(x => x.Coordinate.X).Max() * tileWidth;
+      var height = elements.Select(x => x.Coordinate.Y).Max() * tileWidth;
+      var image = new Bitmap(width, height);
+      var result = await LayTile(elements, tileWidth, image);
+      return result;
+    }
+
+    public async Task<Image> LayTile(IEnumerable<GridElement> gridElements, int tileWidth, Image collector)
+    {
+      if (!gridElements.Any())
+        return collector;
+      await Task.Delay(500);
+
+    };
+  }
+}
